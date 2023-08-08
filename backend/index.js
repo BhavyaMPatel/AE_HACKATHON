32a1de4dffd29fd9f86241ab81cf41f41509e576
BhavyaMPatel <bhavyapatel4012@gmail.com>
2023-08-08 16:18:50 +0530

    backend init - 1

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
 //Backend For Request Resolver
+
+const express = require("express");
+//to parse body from url
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+//Routes
+app.get('/', async (req, res) => {
+    res.send("Hi")
+});
+const port=1000;
+app.listen(port, () => {
+  console.log(`Application live on localhost:{process.env.PORT}`);
+});
